2913fa95d1375fbef7d86373d3847497898f45fc
AyushNaikReal <ayushnaik2506@gmail.com>
2025-11-15 17:58:46 +0530

    odd or even

---
+++ 5.py
@@ -0,0 +1,10 @@
+# chech number is even or odd
+
+no=int(input('Enter any number : '))
+
+# check
+
+if no%2==0:
+    print("number you entered is even ")
+else:
+    print('number you entered is odd')
