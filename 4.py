f9632aa7199cac49fc5fbcd0d4db08fb8873ca28
AyushNaikReal <ayushnaik2506@gmail.com>
2025-11-15 17:53:01 +0530

    Swaping

---
+++ 4.py
@@ -0,0 +1,8 @@
+# python program to swap two numbers whithout using any temporary variable
+
+a=input('Enter any value for a : ')
+b=input('Enter any value for b : ')
+
+# Swaping
+a,b=b,a
+print(f'Value of a is : {a} and value of b is : {b}')
