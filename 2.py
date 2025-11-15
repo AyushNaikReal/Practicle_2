5bf6453a31019dcfe77c27e59e3c97bcd80c76ad
AyushNaikReal <ayushnaik2506@gmail.com>
2025-11-15 17:41:01 +0530

    name and age

---
+++ 2.py
@@ -0,0 +1,6 @@
+# code to take user input as name and age and send them greeting
+
+name=input('Enter your name : ')
+age = input('Enter your age : ')
+
+print(f'Your name is {name} and your age is {age}')
